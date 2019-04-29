29dbe23eb048881bc7ce300a78659cba409b5e2d
michaelleonard15 <michael.leonard@sjsu.edu>
2019-04-28 18:07:20 -0700

    More work on ImplicitParameter

--- ImplicitParameter.js
+++ ImplicitParameter.js
@@ -10,6 +10,16 @@ function createImplicitParameter() {
   let topHeight = DEFAULT_TOP_HEIGHT
   let bounds = { x: 0, y: 0, width: DEFAULT_WIDTH, height: DEFAULT_HEIGHT }
 
+
+  /**
+    Helper function, getTopRectangle called at multiple points
+    in ImplicitParameter.java
+  */
+  function getTopRect() {
+    return  {x: bounds.x, y: bounds.y, 
+             width: bounds.width, height: topHeight}
+  }
+
   return {
 
 
@@ -23,22 +33,18 @@ function createImplicitParameter() {
       return p.x >= bounds.x && p.x <= bounds.x + bounds.width
     },
 
-    ///WHAT DOES THIS DO??
+
     /**                                                                 
       Returns the rectangle at the top of the object node.
       @return the top rectangle
    */
     getTopRectangle: () => {
-      let rect = bounds
-      rect.height = topHeight
-      return  rect
+      return getTopRect()
     },
 
     // WHAT DOES THIS DO???
     getShape: () => {
-      let rect = bounds
-      rect.height = topHeight
-      return  rect
+      return getTopRect()
     },
 
     /**
@@ -62,9 +68,11 @@ function createImplicitParameter() {
     },
 
     /**
-      Set the name of the implicit Parameter
+      Get the name of the implicit Parameter
     */
-    getName: () => {
+    getName: () => 
+    {
+
       return name
     },
 
@@ -78,13 +86,53 @@ function createImplicitParameter() {
 
     getProperties: () => {
       
+    },
+
+
+    translate: (dx, dy) => {
+      bounds.x += dx
+      bounds.y += dy
+    },
+    getBounds: () => {
+      return {x: bounds.x, y: bounds.y, 
+              width: bounds.width, height: bounds.height}
+    },
+
+
+
+    draw: panel => {
+      //Draw the Top box of the implicit parameter
+      const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+      let topRect = getTopRect()
+      rect.setAttribute('x', topRect.x)
+      rect.setAttribute('y', topRect.y)
+      rect.setAttribute('width', topRect.width)
+      rect.setAttribute('height', topRect.height)
+      rect.setAttribute('fill', 'white')
+      rect.setAttribute('stroke', 'black')
+      rect.setAttribute('stroke-width', '1')
+      panel.appendChild(rect)
+
+      // Display the text of the implicit parameter
+      // Needs better formating
+      let text = document.createElementNS('http://www.w3.org/2000/svg', 'text')
+      text.textContent = name
+      text.setAttribute('x', topRect.x)
+      text.setAttribute('y', topRect.y + topRect.height / 2)
+      text.setAttribute('fill', '#000')
+      panel.appendChild(text)
+
+      //Draw the vertical line below the top box.
+      let line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+      line.setAttribute('x1', topRect.x + topRect.width / 2)
+      line.setAttribute('x2', topRect.x + topRect.width / 2)
+      line.setAttribute('y1', topRect.y + topRect.height)
+      line.setAttribute('y2', topRect.y + bounds.height)
+      line.setAttribute('stroke', 'black')
+      line.setAttribute('stroke-width', '1')
+      line.setAttribute('stroke-dasharray', '8 4')
+      panel.appendChild(line)
     }
-
-
-
-
-
-
 
 
 
