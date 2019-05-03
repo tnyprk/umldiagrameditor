84afd168120261227dedae9da7cb93e9ee739d1d
michaelleonard15 <michael.leonard@sjsu.edu>
2019-05-02 20:41:25 -0700

    Call Edge and Point Node

--- CallEdge.js
+++ CallEdge.js
@@ -1,16 +1,20 @@
 'use strict'
 
+
+
 function createCallEdge() {
   let start
   let end
 
 
-  let lineStyle
-  let startArrowHead
-  let endArrowHead
-  let startLabel
-  let middleLabel
-  let endLabel
+  let lineStyle = 'solid'  // or 'dashed'
+  let startArrowHead = undefined
+  let endArrowHead = 'V'
+  let startLabel = undefined
+  let middleLabel = undefined
+  let endLabel = undefined
+
+  let signal = false
   
   return {
 
@@ -30,28 +34,72 @@ function createCallEdge() {
     getEnd() {
       return end
     }, 
+
     getBounds() {
-
+      let points = this.getPoints()
+      if(points.length === 2) {
+          let p1 = points[0]
+          let p2 = points[1]
+          return { x: p1.x, y: p1.y, width: Math.abs(p2.x - p1.x), height: 8} 
+        }
     },
-    getConnectionPoint() {
 
+    getConnectionPoints() {
+      let points = this.getPoints()
+      return { startPoint: points[0], endPoint: points[points.length - 1] }
+    },
+
+    getPoints() {
+      let points = []
+      let s = start.getBounds()
+      let e = end.getBounds()
+
+
+      if(end.getSpecificType() === 'CALLNODE' &&
+          start.getImplicitParameter().equals(end.getImplicitParameter()) ) {
+
+        let p1 = { x: s.x + s.width,  y: e.y - end.getCallYGap() / 2 }
+        let p2 = { x: e.x + e.width,  y: e.y }
+        let p3 = { x: p1.x + e.width, y: p1.y }
+        let p4 = { x: p3.x,           y: p1.y }
+        points.push(p1)
+        points.push(p2)
+        points.push(p3)
+        points.push(p4)
+      }
+      else {
+        let direction = s.x - e.x
+
+        let endPoint = end.getConnectionPoint(direction)
+
+        if(s.x + s.width / 2 < endPoint.x)
+          points.push( { x: s.x + s.width, y: endPoint.y } )
+        else
+          points.push( { x: s.x, y: endPoint.y } )
+        points.push(endPoint)
+      }
+      return points
     },
 
     //////
     // SHAPE EDGE METHODS
     //////
-    getBounds() {
-
-    },
-    contains() {
-
+    contains(p) {
+      let bounds = this.getBounds()
+      return (bounds.x <= p.x && p.x <= bounds.x + bounds.width)
+            && (bounds.y <= p.y && p.y <= bounds.y + bounds.height)
     },
 
     //////
     // SEGMENTED LINE EDGE METHODS
     //////
-    setLineStyle() {},
-    getLineStyle() {},
+
+    setLineStyle(newStyle) {
+      lineStyle = newStyle      // solid or dashed
+    },
+    getLineStyle() {
+      return lineStyle
+    },
     
     setStartArrowHead(newArrowHead) {
       startArrowHead = newArrowHead
@@ -83,15 +131,47 @@ function createCallEdge() {
     getEndLabel() {
       return endLabel
     },
-    draw() {},
-    drawString() {},
-    getAttachmentPoint() {},
-    getStringBounds() {},
-    getBounds() {},
-    getShape() {},
-    getSegmentPath() {},
-    getConnectionPoints() {},
-    getPoints() {},
+
+    setSignal(newValue) {
+      signal = newValue
+    },
+
+    isSignal() {
+      return signal
+    },
+    
+    draw(panel) {
+      const edge = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+      let points = this.getPoints()
+      edge.setAttribute('x1', points[0].x)
+      edge.setAttribute('y1', points[0].y)
+      edge.setAttribute('x2', points[1].x)
+      edge.setAttribute('y2', points[0].y)
+      edge.setAttribute('stroke', 'black')
+      edge.setAttribute('stroke-width', 2)
+      panel.appendChild(edge)
+      if(points.lenght === 4) {
+        const edge2 = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+        edge2.setAttribute('x1', points[0].x)
+        edge2.setAttribute('y1', points[0].y)
+        edge2.setAttribute('x2', points[1].x)
+        edge2.setAttribute('y2', points[0].y)
+        edge2.setAttribute('stroke', 'black')
+        edge2.setAttribute('stroke-width', 2)
+        panel.appendChild(edge2)
+      
+        const edge3 = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+        edge3.setAttribute('x1', points[0].x)
+        edge3.setAttribute('y1', points[0].y)
+        edge3.setAttribute('x2', points[1].x)
+        edge3.setAttribute('y2', points[0].y)
+        edge3.setAttribute('stroke', 'black')
+        edge3.setAttribute('stroke-width', 2)
+        panel.appendChild(edge)
+      }
+    },
+
+
 
     getStart() {
       return start
@@ -101,7 +181,7 @@ function createCallEdge() {
       return end
     },
 
-    
+
     getType() {
       return "EDGE"
     },
@@ -109,7 +189,6 @@ function createCallEdge() {
     getSpecificType() {
       return "CALLEDGE"
     },
-
 
   }
 
---
+++ PointNode.js
@@ -0,0 +1,33 @@
+'use strict'
+
+
+function createPointNode() {
+
+    let x = 0
+    let y = 0
+
+  return {
+
+    draw() {},
+
+    translate(dx, dy) {
+      x += dx
+      y += dy
+    },
+    contains(p) {
+      return ( Math.abs(p.x - x) < 5 &&
+               Math.abs(p.y - y) < 5    )
+    },
+    getBounds() {
+      return { x: x, y: y, width: 0, height: 0}
+    },
+    getConnectionPoint(d) {
+      return { x: x, y: y}
+    },
+    getSpecificType() {
+      return 'POINTNODE'
+    }
+
+
+  }
+}
