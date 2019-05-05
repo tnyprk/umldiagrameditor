a1bb97268651d04864f0d46350883920fcc48fca
tnyprk <tonysj.park97@gmail.com>
2019-05-05 16:14:02 -0700

    noteedge

---
+++ NoteEdge.js
@@ -0,0 +1,50 @@
+function createNoteEdge () {
+    let start
+    let end
+  
+    function center(rect) {
+      return { x: rect.x + rect.width / 2, y: rect.y + rect.width / 2}
+    }
+  
+    return {
+      connect(s, e) {
+        start = s
+        end = e
+      },
+      clone() {
+        return createNoteEdge();
+      },
+
+      draw(panel) {
+        const edge = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+        let sp = start.getConnectionPoint(center(end.getBounds())) // StartPoint
+        let ep = end.getConnectionPoint(center(start.getBounds())) // End Point
+        edge.setAttribute('x1', sp.x)
+        edge.setAttribute('y1', sp.y)
+        edge.setAttribute('x2', ep.x)
+        edge.setAttribute('y2', ep.y)
+        edge.setAttribute('stroke', 'black')
+        edge.setAttribute('stroke-width', 2)
+        edge.setAttribute('stroke-dasharray', '8 4')
+        panel.appendChild(edge)
+      },
+
+      getType() {
+        return "EDGE"
+      },
+      getStart() {
+        return start
+      },
+      getEnd() {
+        return end
+      },
+
+      getSpecificType() {
+        return "NOTEEDGE"
+      }
+
+
+  
+    }
+  }
+  
