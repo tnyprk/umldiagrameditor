054b54a17443b19fc2aff0c7e2d93be81f872069
michaelleonard15 <michael.leonard@sjsu.edu>
2019-05-05 22:35:21 -0700

    More clean up of documentation

--- ImplicitParameter.js
+++ ImplicitParameter.js
@@ -1,6 +1,12 @@
 'use strict'
 
-
+/**
+ * Class to create an ImplicitParameter Node for 
+ * a UML sequence diagram
+ * @constructor
+ *
+ * @returns A new Implicit Parameter Node
+ */
 function createImplicitParameter() {
   let name = 'Hello, World!'
   let DEFAULT_TOP_HEIGHT = 60
@@ -9,43 +15,35 @@ function createImplicitParameter() {
   let topHeight = DEFAULT_TOP_HEIGHT
   let bounds = { x: 0, y: 0, width: DEFAULT_WIDTH, height: DEFAULT_HEIGHT }
   let specificType = 'IMPLICITPARAMETERNODE'
-
-
   let children = []
   let parent = undefined
 
-
   return {
     /**
-      Check that point p is between the min and max x values for this 
-      IplicitPrameter
-      @param p the point to check.
-      @returns true if the point is between the min and max x values, else false.
-    */
+     * Determine if a point is contained in the ImplicitParameter
+     * @param p the point to check.
+     *
+     * @returns true if the point is between the min and max x values, else false.
+     */
     contains(p) {
       return (bounds.x <= p.x && p.x <= bounds.x + bounds.width)
-      //&& (bounds.y <= p.y && p.y <= bounds.y + bounds.height))
     },
 
-
     /**                                                                 
-      Returns the rectangle at the top of the object node.
-      @return the top rectangle
-   */
+     * Returns the rectangle at the top of the ImplicitParameter node.
+     * @return the top rectangle
+     */
     getTopRectangle() {
       return {x: bounds.x, y: bounds.y,
              width: bounds.width, height: topHeight}
     },
 
-    // WHAT DOES THIS DO???
-    getShape() {
-      return this.getTopRectangle()
-    },
-
     /**
-      Gets the connection point for an edge to connect to this node.
-      @param d 
-    */
+     * Gets the connection point for an edge to connect to this node.
+     * @param d Indicator of the direction of the other node
+     *
+     * @returns A point with x & y coordinate to connect the edge to
+     */
     getConnectionPoint(d) {
       if(d > 0)
         return {x: bounds.x + bounds.width, 
@@ -56,20 +54,25 @@ function createImplicitParameter() {
     },
 
     /**
-      Set the name of the implicit Parameter
-    */
+     * Set the name of the implicit Parameter
+     * @param newName the new name for the implicit parameter
+     */
     setName(newName) {
       name = newName
     },
 
     /**
-      Get the name of the implicit Parameter
-    */
+     * Get the name of the implicit Parameter
+     * @returns the name of the implicit Parameter
+     */
     getName() {
       return name
     },
 
-
+    /**
+     * Creates a copy of this implicit parameter
+     * @returns A new ImplicitParameter object with the properties
+     */
     clone() {
       let ret = createImplicitParameter()
       ret.setName(name)
@@ -77,30 +80,49 @@ function createImplicitParameter() {
       return ret
     },
 
-
+    /**
+     * Returns an array with the properties of this object
+     * in the form [name, type, getter, setter] for each property
+     * @returns An array wih the properties of this object.
+     */
     getProperties() {
       return ['Name','text', p1Getter, p1Setter]
     },
 
-
+    /**
+     * Move the implicit parameter by a specific amount 
+     * in the x and y direction
+     * @param dx The x amount to move the implicit parameter
+     * @param dy The y amount to move the implicit parameter
+     */
     translate(dx, dy) {
-      
       bounds.x += dx
-      //bounds.y += dy
       for(const c of children) {
         c.translateFromParent(dx,0)
       }
     },
 
+    /**
+     * Get the boundary box of the ImplicitParameter
+     * @returns A rectangle that bounds the ImplicitParameter.
+     */
     getBounds() {
       return {x: bounds.x, y: bounds.y, 
               width: bounds.width, height: bounds.height}
     },
 
+    /**
+     * Update the boundary box of this ImplicitParameter
+     * @param newBounds The new boundary box
+     */
     updateBounds(newBounds) {
       bounds = newBounds
     },
 
+    /**
+     * Draws the ImplicitParameter
+     * @param The panel to draw the ImplicitParameter on.
+     */
     draw(panel) {
       //Draw the Top box of the implicit parameter
       const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')       
@@ -122,7 +144,6 @@ function createImplicitParameter() {
       text.setAttribute('y', topRect.y + topRect.height / 2)
       text.setAttribute('fill', '#000')
       
-
       //Draw the vertical line below the top box.
       let line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
       line.setAttribute('x1', topRect.x + topRect.width / 2)
@@ -140,18 +161,34 @@ function createImplicitParameter() {
 
     },
 
+    /**
+     * Return the parent of this node
+     * @returns The parent of this node
+     */
     getParent() {
       return parent
     },
 
+    /**
+     * Set the parent of this node
+     * @param node the node that is the parent of this node
+     */
     setParent(node) {
       parent = node
     },
 
+    /**
+     * Returns the children of this node
+     * @returns An array with the children of this node
+     */
     getChildren() {
       return children
     },
 
+    /**
+     * Add a child to this node
+     * @param c the new child node
+     */
     addChild(c) {
       if(c.getSpecificType() === "CALLNODE"){
         children.push(c)
@@ -159,9 +196,14 @@ function createImplicitParameter() {
         console.log(mid)
         c.translateFromParent(mid - (c.getBounds().width/2),0)
       }
-
     },
 
+    /**
+     * Provides a method for this nodes parent to move
+     * this node
+     * @param dx The x amount to move the implicit parameter
+     * @param dy The y amount to move the implicit parameter
+     */
     translateFromParent(dx,dy) {
       bounds.x += dx    
       bounds.y += dy
@@ -170,6 +212,10 @@ function createImplicitParameter() {
       }
     },
 
+    /**
+     * Remove a node from the list of this nodes children
+     * @param The node to be reomved
+     */
     removeChild(node)
     {
       for(i = 0; i < children.length; i++){
@@ -181,6 +227,10 @@ function createImplicitParameter() {
       node.setParent
     },
 
+    /**
+     * Remove a edge from the list of this nodes children
+     * @param The edge to be reomved
+     */
     removeNode(e)
     {
        if (e === parent) parent = null; 
@@ -189,20 +239,29 @@ function createImplicitParameter() {
        } 
     },
 
+    /**
+     * Returns the type of this object 
+     * @returns the type of this object 'NODE'
+     */
     getType() {
       return 'NODE'
-      },
-  
+    },
+
+    /**
+     * Returns the specific type of this object 
+     * @returns the specific type of this object 'IMPLICITPARAMETERNODE'
+     */  
     getSpecificType() {
       return 'IMPLICITPARAMETERNODE'
     },
   
-
+    /**
+     * Returns an array with the properties of this object
+     * in the form [name, type, getter, setter] for each property
+     * @returns An array wih the properties of this object.
+     */
     getProperties() {
       return ['Name', 'text', this.getName, this.setName]
     }
-
-
-
   }
 }
