5b29e4264dea4370638963e6550c639495ef57e7
michaelleonard15 <michael.leonard@sjsu.edu>
2019-05-05 18:35:47 -0700

    lineEdge contains methos

--- graphed.js
+++ graphed.js
@@ -52,8 +52,8 @@ function Graph() {
       @return an edge containing p or null if no edges contain p
    */
     findEdge(point) {
-      for (let i = edges.size() - 1; i >= 0; i--) {
-        const e = edges.get(i);
+      for (let i = edges.length - 1; i >= 0; i--) {
+        const e = edges[i];
         if (e.contains(point)) return e;
       }
       return undefined;
